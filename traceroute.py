1bce66330a539522e65caadc7125d3386c7dfca7
Olimar <gpfernandezflorio@gmail.com>
2016-05-24 11:35:22 -0300

    Arreglo algunas cositas, ahora que puedo probarlo.

--- traceroute.py
+++ traceroute.py
@@ -1,5 +1,6 @@
 import sys
 import datetime
+import math
 from scapy.all import *
 
 if __name__ == '__main__':
@@ -8,45 +9,59 @@ if __name__ == '__main__':
 	else:
 		dest = 'www.dc.uba.ar'
 
-#	traceroute(dest)
+	#traceroute(dest)
 	limit = 25
 	tries = 5
 
 	# Medidas
 	ips = []
 	averages = []
-	for ttlive in range(1,limit+1)
+	for ttlive in range(1,limit+1):
 		times = []
+		ip = "Unknown"
 		for i in range(0,tries):
 			a = datetime.datetime.now()
+			# PONER UN TIMEOUT!! a veces se queda esperando infinitamente
 			pkt = sr(IP(dst=dest, ttl=ttlive) / ICMP())
 			b = datetime.datetime.now()
 			delta = b-a
 			times.append(delta.seconds*1000 + delta.microseconds/1000)
-			ips.append(pkt[0][IP].src)
-			pkt[0][ICMP].display()
-		averages.append(sum(times)/len(times))
-		pkt[0][ICMP].type
-		if (pkt[0][ICMP].type == "echo-reply"):
+			#if NOT(timeout):
+			ip = pkt[0][0][1].src
+		if (ip == "Unknown"):
+			print("Error")
+			sys.exit()
+		ips.append(ip)
+		print("Times " + str(ttlive) + ": " + str(times))##
+		avg = sum(times)/len(times)
+		print(" -> Avg: " + str(avg))##
+		averages.append(avg)
+		if (pkt[0][0][1].type == 0):
 			break
 
-	# Cálculos
-	relatives = [(averages[0],ips[0])]
+	print("IPs: " + str(ips))##
+	print("Times: " + str(averages))##
+
+	# Calculos
+	relatives = [(0,ips[0])] # Podemos asumir que el tiempo al primer hop es nulo*
 	for i in range(1,len(averages)):
 		relatives.append((averages[i]-averages[i-1],ips[i]))
-	relatives
-	relatives.sort(key=lambda x : x[0]) # Ordeno según el tiempo relativo
-	relatives
-	sample_mean = sum(relatives)/len(relatives)
+	relatives.sort(key=lambda x : x[0]) # Ordeno segun el tiempo relativo
+	sample_mean = sum(map(lambda x: x[0], relatives))/len(relatives)
+	print("Average: " + str(sample_mean))##
 	sample_sd = 0
-	for r in relatives:
+	for r in map(lambda x: x[0], relatives):
 		sample_sd += (r - sample_mean)*(r - sample_mean)
-	sample_sd = sqrt(sample_sd/(len(relatives)-1))
+	sample_sd = math.sqrt(sample_sd/(len(relatives)-1))
+	print("std Deviation: " + str(sample_sd))##
 	taus = [0,0,1.1511,1.4250,1.5712,1.6563,1.7110,1.7491,1.7770,1.7984,1.8153,1.8290,1.8403,	
 		1.8498,1.8579,1.8649,1.8710,1.8764,1.8811,1.8853,1.8891,1.8926,1.8957,1.8985,1.9011]
 	tau = taus[len(relatives)]
 	outliers = []
 	for i in range(0,len(relatives)):
-		if (abs(relatives[i]-sample_mean) > tau*sample_sd):
-			print("Outlier (" + i + "): " + relatives[i])
+		if (abs(relatives[i][0]-sample_mean) > tau*sample_sd):
+			print("Outlier (" + relatives[i][1] + "): " + str(relatives[i][0]))
 			outliers.append(i)
+	print("Outliers: " + str(outliers))
+
+#*: Si no, estamos agregándole al primer hop el tiempo de procesamiento incluído en construír el paquete y enviarlo.
