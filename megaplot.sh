b15ce2542c8eda8b7ec59bf3e39833ac41e6d0ef
Olimar <gpfernandezflorio@gmail.com>
2016-06-08 12:58:16 -0300

    Fix Megaplot

--- megaplot.sh
+++ megaplot.sh
@@ -1,59 +1,94 @@
-mkdir tokyo
+#Creo los directorios (comentar si ya existen)
+mkdir tokyo india sydney milan saopaulo congestion
+
+#Muevo los archivos
+FILES=*congestion.dat
+for f in $FILES
+do
+	git mv $f congestion/$f
+done
+FILES=www.u-tokyo.ac.jp*
+for f in $FILES
+do
+	git mv $f tokyo/$f
+done
+FILES=www.iitkgp.ac.in*
+for f in $FILES
+do
+	git mv $f india/$f
+done
+FILES=sydney.edu.au*
+for f in $FILES
+do
+	git mv $f sydney/$f
+done
+FILES=www.unimi.it*
+for f in $FILES
+do
+	git mv $f milan/$f
+done
+FILES=www5.usp.br*
+for f in $FILES
+do
+	git mv $f saopaulo/$f
+done
+
+#Genero los GNUplots (Tokyo)
+cd tokyo
 FILES=www.u-tokyo.ac.jp*.cor
 S='''set terminal png size 1920,1080 background rgb "#aaaaff"\nset output "mega_tokyo.png"\nset size 1,1\n\nplot "world.dat" with filledcurves ls 1 lc rgb "#aaffaa", \ \n  "" with l ls 2'''
 for f in $FILES
 do
 	T=", \ \n  \"$f\" with linespoint pt 7 lw 2"
 	S="$S$T"
-	mv $f tokyo/$f
 done
+echo $S > mega_tokyo.gnuplot
+cd ..
 
-echo $S >> tokyo/mega_tokyo.gnuplot
-
-mkdir india
+#Genero los GNUplots (India)
+cd india
 FILES=www.iitkgp.ac.in*.cor
 S='''set terminal png size 1920,1080 background rgb "#aaaaff"\nset output "mega_india.png"\nset size 1,1\n\nplot "world.dat" with filledcurves ls 1 lc rgb "#aaffaa", \ \n  "" with l ls 2'''
 for f in $FILES
 do
 	T=", \ \n  \"$f\" with linespoint pt 7 lw 2"
 	S="$S$T"
-	mv $f india/$f
 done
+echo $S > mega_india.gnuplot
+cd ..
 
-echo $S >> india/mega_india.gnuplot
-
-mkdir sydney
+#Genero los GNUplots (Sydney)
+cd sydney
 FILES=sydney.edu.au*.cor
 S='''set terminal png size 1920,1080 background rgb "#aaaaff"\nset output "mega_sydney.png"\nset size 1,1\n\nplot "world.dat" with filledcurves ls 1 lc rgb "#aaffaa", \ \n  "" with l ls 2'''
 for f in $FILES
 do
 	T=", \ \n  \"$f\" with linespoint pt 7 lw 2"
 	S="$S$T"
-	mv $f sydney/$f
 done
+echo $S > mega_sydney.gnuplot
+cd ..
 
-echo $S >> sydney/mega_sydney.gnuplot
-
-mkdir milan
+#Genero los GNUplots (Milán)
+cd milan
 FILES=www.unimi.it*.cor
 S='''set terminal png size 1920,1080 background rgb "#aaaaff"\nset output "mega_milan.png"\nset size 1,1\n\nplot "world.dat" with filledcurves ls 1 lc rgb "#aaffaa", \ \n  "" with l ls 2'''
 for f in $FILES
 do
 	T=", \ \n  \"$f\" with linespoint pt 7 lw 2"
 	S="$S$T"
-	mv $f milan/$f
 done
+echo $S > mega_milan.gnuplot
+cd ..
 
-echo $S >> milan/mega_milan.gnuplot
-
-mkdir saopaulo
+#Genero los GNUplots (Sao Paulo)
+cd saopaulo
 FILES=www5.usp.br*.cor
 S='''set terminal png size 1920,1080 background rgb "#aaaaff"\nset output "mega_saopaulo.png"\nset size 1,1\n\nplot "world.dat" with filledcurves ls 1 lc rgb "#aaffaa", \ \n  "" with l ls 2'''
 for f in $FILES
 do
 	T=", \ \n  \"$f\" with linespoint pt 7 lw 2"
 	S="$S$T"
-	mv $f saopaulo/$f
 done
-
-echo $S >> saopaulo/mega_saopaulo.gnuplot
+echo $S > mega_saopaulo.gnuplot
+cd ..
